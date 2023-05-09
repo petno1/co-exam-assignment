cf7c5151f16185fd61bb0b35a975097b772e5f5d
Jessie Lonsdale <lonsdale.jess@gmail.com>
2023-05-09 19:46:46 +0200

    runs but doesnt converge on correct value

--- InteriorPoint/InteriorPointDriver.m
+++ InteriorPoint/InteriorPointDriver.m
@@ -1,6 +1,6 @@
 %% Step 1 - prep the inputs
 n=20;
-[H, g, A, b, C, dl, du, l, u] = randomQPGenerator(n,0.50);
+[H, g, A, b, C, dl, du, l, u] = randomQPGenerator(n,0.90);
 % Convert dl <= C' x <= cu, l <= x <= u to c(x) = Cbar' x + dbar >= 0
 Cbar = [C -C eye(n,n) -eye(n,n)];
 dbar = [-dl; du; -l; u];
@@ -11,5 +11,7 @@ dbar = [-dl; du; -l; u];
 [x0] = linprog(zeros(1,n)',[A,Cbar]',[b;dbar]);
 
 [x] = InteriorPointQP(H, g, A, b, Cbar, dbar, x0);
+[x2] = quadprog(H, g', Cbar',dbar,A',b);
 
-
+0.5*x'*H*x-g'*x
+0.5*x2'*H*x2-g'*x2
--- InteriorPoint/InteriorPointQP.m
+++ InteriorPoint/InteriorPointQP.m
@@ -5,34 +5,30 @@ function [x_sol] = InteriorPointQP(H, g, A, b, C, d, x0)
 %   C' x + d + s = 0
 %   s > 0
 
-
 % Sets constants for the algorithm
-mIn = length(d);
-m = length(b);
-n = length(x0);
+mIn = length(x0);
 tol = 0.000001;
-eta = 0.995;
+eta = 0.99;
 
 y0 = zeros(size(b));
 z0 = ones(size(d));
 s0 = ones(size(d));
-
 
 x=x0;
 y=y0; %(lambda)
 z=z0; %(mu)
 s=s0;
 
+S = diag(s);
+Z = diag(z);
+e = ones(size(s));
 
 rL = H*x+g-A*y-C*z;
 rA = -A'*x+b;
 rC = -C'*x+s+d;
+rSZ = S*Z*e;
 
-S = diag(s);
-M = diag(z);
-e = ones(size(s));
-
-rSm = S*M*e;
+dualGap = (z'*s)/(mIn);
 
 % Converged
 Converged = (norm(rL,inf) <= tol) && ...
@@ -42,49 +38,83 @@ Converged = (norm(rL,inf) <= tol) && ...
 %%       
 max_iter = 100;
 iter = 0;
-
-dx = -rL;
-dy = -rA;
-dz = -rC;
-ds = -rSm;
+hist = [];
 
 while ~Converged && (iter<max_iter)
     iter = iter+1;
  
-    rl_bar = rL - C*(M/S)*(rC-inv(M)*rSm);
-    h_bar = H + C*(M/S)*C';
-    KKT = [h_bar -A; -A' zeros(m)];
-    [L,D,~] = ldl(KKT,'vector');    
-    rhs = -[rl_bar ; rA ];
-    solution = L'\(D\(L\rhs));
-    dx = solution ( 1 : length (x) )';
-    dy = solution (length(x)+1:length(x)+length(y))'; 
-    dz = (M/S)*C'.*dx + (M/S)*(rC-inv(M)*rSm);
-    ds =  (inv(M)*rSm)-(inv(M)*S*dz);
-    
-    dZS = [dz; ds];
+    disp('===================')
+    iter
+    disp('===================')
+
+    h_bar = H + C*(Z/S)*C';
+    rl_bar =  rL-C*(S\Z)*(rC-Z\rSZ);
+    KKT = [h_bar -A; -A' zeros(size(A,2))];
+    [L,D,p] = ldl(KKT,"lower",'vector');    
+    rhs = -[rl_bar;rA];
+    solution(p) = L'\(D\(L\rhs(p)));
+
+    %% Affine Direction
+    dx_affine = solution(1:length(x))';
+    dz_affine =-(S\Z)*C'*dx_affine+(S\Z)*(rC-Z\rSZ);
+    ds_affine = -(Z\rSZ)-(Z\(S*dz_affine));
+
+    dZS = [dz_affine; ds_affine];
     alphas = (-[z; s] ./ dZS);
-    alpha = min([1; alphas(dZS < 0)]);
-    alphaBar = eta * alpha;
+    alphaMax_affine = eta*min([1; alphas(dZS < 0)]);
+
+    %% Duality gap and centering parameter
+    affineDualGap = ((z+alphaMax_affine*dz_affine)'*(s+alphaMax_affine*ds_affine))/(mIn);
+    sigma = (affineDualGap./dualGap)^3;
+
+    %% Affine-Centering-Correction Direction
+    rSZ_bar = rSZ + (ds_affine.*dz_affine.*e) - (affineDualGap*sigma.*e);
+    rl_bar = rL- C*(S\Z)*(rC-Z\rSZ_bar);
+
+    [L,D,p] = ldl(KKT,'vector'); 
+
+    rhs = -[rl_bar ; rA];
+    solution(p) = L'\(D\(L\rhs(p)));
+
+    dx = solution(1:length(x))';
+    dy = solution(length(x)+1:length(x)+length(y))';
     
+    dz = -(S\Z)*C'*dx+(S\Z)*(rC-Z\rSZ_bar);
+    ds = -(Z\rSZ_bar)-(Z\(S*dz));
+
+    dZS = [dz;ds];
+    alphas =(-[z;s]./dZS);
+    alpha = min([1; alphas(dZS<0)])*eta;
+
     % Update of position
-    x = x + alphaBar * dx
-    y = y + alphaBar * dy
-    z = z + alphaBar * dz
-    s = s + alphaBar * ds
+    x = x + alpha * dx;
+    y = y + alpha * dy;
+    z = z + alpha * dz;
+    s = s + alpha * ds;
 
+    S = diag(s);
+    Z = diag(z);
+    e = ones(size(s));
 
-    
+    rL = H*x+g-A*y-C*z;
+    rA = -A'*x+b;
+    rC = -C'*x+s+d;
+    rSZ = S*Z*e;
 
+    dualGap = (z'*s)/(mIn);
 
+    Converged = (norm(rL,inf) <= tol) && ...
+            (norm(rA,inf) <= tol) && ...
+            (norm(rC,inf) <= tol) && ...
+            (abs(s) <= tol);
 
+hist = vertcat(hist, 0.5*x'*H*x-g'*x);
 
 end
 
-
-
-
-x_sol = 0
+hist
+iter
+x_sol = x;
 
 end
 
